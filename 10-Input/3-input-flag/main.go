7553c2f1c1f10201c8e8ee9970a898d0381df625
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-05-28 13:36:57 -0400

    feat: Added examples input-args and input-flag.

---
+++ 10-Input/3-input-flag/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+)
+
+// command-line 'flags' specify options for command-line programs
+
+// 'flag' provides functions for command-line flag parsing
+
+// flag functions: 'String()', 'Bool()', 'Int()', etc.
+// each function requires 3 arguments: (name, default value, help message)
+
+// The return value of a flag function is the address of a variable (pointer) that stores the value of the flag
+ 
+func main() {
+
+	// possible to declare an option that uses an existing 'var' declared elsewhere in the program
+	// must pass in a pointer to the flag declaration function
+	var strVar string
+	flag.StringVar(&strVar, "strVar", "foobar", "help message for strVar")
+
+	var intVar int
+	flag.IntVar(&intVar, "intVar", 1234, "help message for intVar")
+
+	generateCpuProfilePointer := flag.String("generateProfile", "profile.profile", "Emit a profile for debugging")
+	maxNodeModuleJsDepthPointer := flag.Int("maxDepth", 0, "Specify the maximum depth")
+	allowJsPointer := flag.Bool("allowFiles", false, "Allow files to be a part of your program")
+
+	// parse the command-line flags
+	// called after all flags are defined and before flags are accessed by the program
+	flag.Parse()
+
+	fmt.Println("strVar:", strVar)
+	fmt.Println("intVar:", intVar)
+
+	fmt.Println("generateProfile:", *generateCpuProfilePointer)
+	fmt.Println("maxDepth:", *maxNodeModuleJsDepthPointer)
+	fmt.Println("allowFiles:", *allowJsPointer)
+	fmt.Println("tail:", flag.Args()) // output any remaining input
+}
+
+//	% go run main.go -strVar=stringFlag -intVar=99999999999 a1 a2 a3
+//	strVar: stringFlag
+//	intVar: 99999999999
+//	generateProfile: profile.profile
+//	maxDepth: 0
+//	allowFiles: false
+//	tail: [a1 a2 a3]
